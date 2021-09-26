c68c35155db5664f0c8f6e48579e40aefc5440de
StakeHub <83736116+StakeHubFinance@users.noreply.github.com>
2021-09-25 20:26:48 -0400

    Update BabyBoomerNFT.sol

--- BabyBoomerNFT.sol
+++ BabyBoomerNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC721.sol";
+import "./Ownable.sol";
 
 contract BabyBoomerNFT is ERC721, Ownable {
     
